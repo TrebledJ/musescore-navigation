6bfb2019d2a318f4b6fe3926db0d76f99a96b605
Johnathan <jjjlaw@connect.ust.hk>
2022-08-24 19:50:28 +0800

    refactor js util into separate module

--- bookmark/bookmarks.js
+++ bookmark/bookmarks.js
@@ -1,4 +1,4 @@
-
+.import "../utils.js" as Utils
 
 // Wrapping options for select-prev / select-next.
 var WrapSelect = {
@@ -18,6 +18,7 @@ function BookmarkCursor(onInfo, onError) {
     this.bookmarkText = "bkmk";
     this.bookmarkFontSize = 0.5;
     this.allowedElements = [Element.CHORD, Element.REST, Element.NOTE];
+    // this.Utils = Utils;
 
     // Private.
     this.onInfo = onInfo || console.log;
@@ -169,8 +170,7 @@ BookmarkCursor.prototype.foreachBookmark = function (direction, cursor, bkmkCall
  */
 BookmarkCursor.prototype.getSelectableUnderElement = function (element) {
     // Get the chord/note/rest under the bookmark.
-    var staves = getStaves(this.cursor.segment);
-    var staffIdx = indexOfStaff(staves, element);
+    var staffIdx = Utils.getStaffIdx(this.cursor.segment, element);
     var selectable = this.cursor.segment.elementAt(4 * staffIdx);
 
     if (selectable && selectable.type == Element.CHORD) {
@@ -210,126 +210,5 @@ BookmarkCursor.prototype.clearAllBookmarks = function () {
  * @return  An MS cursor or null.
  */
 BookmarkCursor.prototype.getCursorAtSelection = function () {
-    return getCursorAtSelection(this.allowedElements, this.onError);
-}
-
-// ---------------------------------------------------------------
-
-/**
- * @brief   Get a MS cursor at the selected note or elements.
- * @return  An MS cursor or null.
- */
- function getCursorAtSelection(allowedElements, onError) {
-    // 1. Cursor rewind to selection start and get the first selected element.
-    // 2. If the element is valid, then we're done; we've our cursor.
-    // 3.a. If no element selected, check curScore.selection. This means an individual note/rest is selected.
-    // 3.b. Get the element from curScore.selection.
-    // 4. Get the segment from the element.
-    // 5. Advance cursor to said segment.
-    // 6. Set correct staffIdx as selected element.
-
-    var cursor = curScore.newCursor();
-
-    // 1.
-    cursor.rewind(Cursor.SELECTION_START);
-
-    if (!cursor.segment && !cursor.element && curScore.selection.elements.length === 0) {
-        onError(qsTr("Please select a note or rest, and try again."));
-        return null;
-    }
-
-    // 2.
-    var e = cursor.element;
-    if (e) {
-        if (!includes(allowedElements, e.type)) {
-            onError(qsTr("Selection isn't note or rest."));
-            return null;
-        }
-        console.log("found cursor from range selection");
-        return cursor;
-    }
-
-    console.log("could not find cursor...");
-    console.log("falling back to analysing curScore.selection...");
-
-    // 3.
-    var es = curScore.selection.elements;
-    if (es.length == 0) {
-        return null;
-    }
-    e = es[0];
-
-    console.log("element: %1 / %2".arg(e.type).arg(e.name));
-    if (!includes(allowedElements, e.type)) {
-        onError(qsTr("Selection isn't note or rest."));
-        return null;
-    }
-
-    // 4.
-    var seg = getParentSegment(e);
-    if (!seg) {
-        onError("could not get parent segment of %1".arg(e));
-        return null;
-    }
-
-    // 5.
-    cursor.rewind(Cursor.SCORE_START);
-    while (cursor.segment) {
-        if (cursor.segment.is(seg)) {
-            console.log("found matching segment");
-            break;
-        }
-        cursor.next();
-    }
-
-    if (!cursor.segment) {
-        onError("could not find matching segment for element from curScore.selection (%1)".arg(e));
-        return null;
-    }
-
-    // 6.
-    var staves = getStaves(seg);
-    var staffIdx = indexOfStaff(staves, e);
-    cursor.staffIdx = staffIdx;
-    console.log("staffIdx: %1 / num: %2".arg(staffIdx).arg(staves.length));
-
-    return cursor;
-}
-
-function getParentSegment(e) {
-    // Try walking up parent hierarchy within a limit and finding a segment parent.
-    var limit = 5;
-    for (var i = 0; e && i < limit; i++) {
-        if (e.type === Element.SEGMENT) // Found segment parent.
-            return e;
-        e = e.parent;
-    }
-    return null; // Could not find.
-}
-
-function includes(array, element) {
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] === element)
-            return true;
-    }
-    return false;
-}
-
-function getStaves(seg) {
-    var staves = [];
-    for (var i = 0; ; i++) {
-        var e = seg.elementAt(4 * i);
-        if (!e)
-            break;
-        staves.push(e.staff);
-    }
-    return staves;
-}
-
-function indexOfStaff(staves, e) {
-    for (var i = 0; i < staves.length; i++) {
-        if (staves[i].is(e.staff))
-            return i;
-    }
-    return -1;
+    return Utils.getCursorAtSelection(this.allowedElements, this.onError);
 }
